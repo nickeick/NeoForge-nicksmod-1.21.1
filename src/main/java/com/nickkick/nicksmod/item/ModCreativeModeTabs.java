4c70a4c7fde5630d42f7cd6cef217027c8f430a7
Nick Eickhoff <nickeick@comcast.net>
2025-05-21 02:17:59 -0500

    Added blocks and creative mode tabs

--- src/main/java/com/nickkick/nicksmod/item/ModCreativeModeTabs.java
+++ src/main/java/com/nickkick/nicksmod/item/ModCreativeModeTabs.java
@@ -22,6 +22,7 @@ public class ModCreativeModeTabs {
                     .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModItems.BISMUTH);
                         output.accept(ModItems.RAW_BISMUTH);
+                        output.accept(ModItems.CHISEL);
                     })
                     .build());
 
@@ -32,6 +33,7 @@ public class ModCreativeModeTabs {
                     .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModBlocks.BISMUTH_BLOCK);
                         output.accept(ModBlocks.BISMUTH_ORE);
+                        output.accept(ModBlocks.BISMUTH_DEEPSLATE_ORE);
                     })
                     .build());
 
--- src/main/java/com/nickkick/nicksmod/item/ModItems.java
+++ src/main/java/com/nickkick/nicksmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.nickkick.nicksmod.item;
 
 import com.nickkick.nicksmod.NicksMod;
+import com.nickkick.nicksmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -13,6 +14,9 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
             () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
